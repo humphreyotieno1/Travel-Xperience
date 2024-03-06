a0630b9902865c20005b18a687a7527c4b9b78a2
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-03-07 00:24:01 +0300

    Styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,10 +64,10 @@ function Footer() {
         <div className='social-media-wrap'>
           <div className='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL <i className="fab fa-gripfire"></i>
+              TravelXperience <i className="fab fa-gripfire"></i>
             </Link>
           </div>
-          <small className='website-rights'>TRVL © 2022</small>
+          <small className='website-rights'>TravelXperience © 2022</small>
           <div className='social-icons'>
             <Link
               className='social-icon-link facebook'
